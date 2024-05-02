fd396ad7635c6d72d763f37b86513fc194c9ab38
Warren Ochibobo <ochibobowarren@gmail.com>
2024-05-02 19:34:25 +0300

    Began implementing the BalanceSheet

--- src/journal/balance_sheet.rs
+++ src/journal/balance_sheet.rs
@@ -1,7 +1,15 @@
+use crate::journal::ledger::Ledger;
+use chrono::{DateTime, Utc};
 ///
 /// `Balance Sheet` structure
 /// This is basically a structured view of the ledger
 ///
 pub struct BalanceSheet {
     id: usize,
+    from_date: DateTime<Utc>,
+    to_date: DateTime<Utc>,
+    ledger: Ledger,
 }
+
+// BalanceSheet implementation
+impl BalanceSheet {}
