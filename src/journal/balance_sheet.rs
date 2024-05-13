da7dc945133d552d2714e739ec34501e766a4ba5
Warren Ochibobo <ochibobowarren@gmail.com>
2024-05-13 17:37:37 +0300

    Initial hacky implementation for calculating the aggregates for accounts based on transaction entries in the BalanceSheet build method

--- src/journal/balance_sheet.rs
+++ src/journal/balance_sheet.rs
@@ -1,6 +1,8 @@
-use crate::journal::accounting_tree::AccountTree;
-use crate::journal::ledger::{Ledger, LedgerReader};
+use crate::journal::accounting_tree::{AccountTree, ActionType};
+use crate::journal::ledger::{EntryType, Ledger, LedgerReader, TransactionEntry};
 use chrono::{DateTime, Utc};
+use std::borrow::Borrow;
+use std::{collections::HashMap, rc::Rc};
 
 ///
 /// `Balance Sheet` structure
@@ -129,9 +131,95 @@ impl BalanceSheet {
         &self.ledger
     }
 
-    //
-    // Build the AccountTree by populating it with values from the ledger
-    //    - Call this method build_tree or generate or execute etc
+    ///
+    /// Build the AccountTree by populating it with values from the ledger
+    ///    - Call this method build_tree or generate or execute etc
+    ///    - Should return a Result<Ok, Err>
+    ///
+    pub fn build(&self) {
+        // Retieve all transaction entries
+        let transaction_entries: Vec<&Rc<TransactionEntry>> = self
+            .ledger
+            .journal_entries()
+            .iter()
+            .map(|journal_entry| journal_entry.transaction_entries())
+            .flatten()
+            .collect();
+
+        // Group by account type
+        // Collect all the unique account types by name
+        let mut accounts_aggregate_map: HashMap<String, f64> = HashMap::new();
+
+        // Perform aggregates for each account type taking credit/debit into consideration
+        for transaction_entry in transaction_entries.iter() {
+            // Perform the aggregations based on the Credit/Debit rule per account type
+            let acc_type_ref = transaction_entry.account_type();
+            let account_type_option = acc_type_ref.borrow().as_ref();
+            let account_type;
+            let acc_name = transaction_entry.account_name().clone();
+
+            // Attempt to retieve the primary account type
+            // This helps in knowing whether to increase/decrease the amount of an account
+            // when the transaction is a credit/debit.
+            match account_type_option {
+                None => {
+                    panic!(
+                        "Transaction with id: {:?} and account name: {:?} has account_type of None",
+                        1, acc_name,
+                    );
+                }
+                Some(account_type_result) => {
+                    account_type = account_type_result.clone().to_owned();
+                }
+            }
+
+            // Get the amount associated with the account name under the transaction entry or
+            // initialize it to 0
+            if !accounts_aggregate_map.contains_key(&acc_name) {
+                accounts_aggregate_map.insert(acc_name.clone(), 0f64);
+            }
+
+            // Get the sign that'll be assigned to the amount
+            let mut signum = 0f64;
+
+            // Increase of decrease the amounts accordingly
+            match transaction_entry.entry_type() {
+                EntryType::Credit => match account_type.on_credit() {
+                    ActionType::Decrease => {
+                        signum = -1f64;
+                    }
+                    ActionType::Increase => {
+                        signum = 1f64;
+                    }
+                },
+                EntryType::Debit => match account_type.on_debit() {
+                    ActionType::Decrease => {
+                        signum = -1f64;
+                    }
+                    ActionType::Increase => {
+                        signum = 1f64;
+                    }
+                },
+            }
+
+            // Apply the signum to the amount
+            let transaction_amount = transaction_entry.amount() * signum;
+
+            // Apply this amount delta in the accounts aggregate map
+            accounts_aggregate_map.insert(
+                acc_name.clone(),
+                accounts_aggregate_map.get(&acc_name).unwrap() + transaction_amount,
+            );
+        }
+
+        // Map the account name to an account tree node
+        //  - Retrieve the tree node from a cache
+        //  - In case of a miss, perform a DFS to retrieve the tree node
+        //  - In case the account_name has no matching node, throw an error immeadiately
+        // Use the aggregates to compute amounts that'll be propagated up the tree
+        //  - Propagate the amounts up the tree.
+        // Assert Assets = Liabilities + Equity
+    }
     // Consider adding methods to retrieve account trees with subtotals
     // Consider adding a method to return the IncomeStatement
     // Consider adding a method to return the CashflowStatement
